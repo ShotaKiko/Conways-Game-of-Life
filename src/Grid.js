10416a2947d09a3688c2855cf35266c5853b4dda
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-06-24 18:33:18 -0500

    simulation now runs off of manual cell input

--- src/Grid.js
+++ src/Grid.js
@@ -1,10 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useRef } from 'react';
 import './App.css';
 
 import produce from 'immer'
 
-const numOfRows= 30
-const numOfColumns = 60
+const numOfRows= 20
+const numOfColumns = 40
+
+const neighborCoordinates = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [-1, 0],
+  [1, 0]
+]
 
 function Grid() {
 
@@ -18,7 +29,42 @@ function Grid() {
 
   const [isSimulationRunning, setSimulationRunning] = useState(false)
 
+  const runningRef = useRef(isSimulationRunning)
+  runningRef.current = isSimulationRunning
+
   // console.log(grid)
+
+  const runSimulation = useCallback(() => {
+    //simulate and see if we are running
+    if(!runningRef.current){
+      return //kill condition
+    }
+
+    setGrid(g => {
+      return produce(g, gridCopy => {
+        for (let r= 0; r < numOfRows; r++) {
+          for(let c = 0; c < numOfColumns; c++) {
+            let neighborsCount = 0 //dead cells
+            neighborCoordinates.forEach(([x, y]) => {
+              const newR = r + x
+              const newC = c + y
+              if (newR >= 0 && newR < numOfRows && newC >= 0 && newC < numOfColumns) {
+                neighborsCount += g[newR][newC]
+              }
+            })
+            if (neighborsCount < 2 || neighborsCount > 3) {
+              gridCopy[r][c] = 0
+            } else if (g[r][c] === 0 && neighborsCount === 3) {
+              gridCopy[r][c] = 1
+            }
+          }
+        }
+      })
+    })
+
+    setTimeout(runSimulation, 500)
+
+  }, [])
 
 
   return (
@@ -26,6 +72,10 @@ function Grid() {
     {/* if sim is running we will display stop otherwise start if sim is not running */}
     <button onClick={() => {
         setSimulationRunning(!isSimulationRunning)
+        if (!isSimulationRunning) {
+          runningRef.current = true
+          runSimulation()
+        }
     }}>
         {isSimulationRunning ? "Stop Simulation" : "Start Simulation"}
     </button>
@@ -44,6 +94,7 @@ function Grid() {
                     gridCopy[rowIndex][colIndex] = grid[rowIndex][colIndex] ? 0 : 1
                 })
                 setGrid(newGrid)
+                console.log(newGrid)
             }}
           style={{ 
             width: 20, 
