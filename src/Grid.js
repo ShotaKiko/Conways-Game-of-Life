313817474ca8b1eaef8a638e05242a11e8e96e57
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-06-28 23:57:36 -0500

    added a sim speed slider

--- src/Grid.js
+++ src/Grid.js
@@ -1,9 +1,13 @@
 import React, { useState, useCallback, useRef } from 'react';
 import './App.css';
+import Sonic from "./icons/sonic.png"
+import Snorlax from "./icons/snorlax.png"
 
 import produce from 'immer'
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
+import Slider from '@material-ui/core/Slider';
+import SpeedGrid from '@material-ui/core/Grid';
 
 const unactiveButtonColor = "steelBlue"
 const unactiveButtonFontColor = "#b1f1fa"
@@ -12,6 +16,17 @@ const activeButtonColor = "#b1f1fa"
 const activeButtonFontColor = "steelBlue"
 
 const useStyles = makeStyles({
+  root:{
+    // border:"1px solid pink",
+    width:"400px",
+    display:"flex",
+    alignItems:"center",
+    justifyContent:"center",
+  },
+  slider:{
+    width:"210px",
+    // border:"1px solid white"
+  },
   booton:{
       color: unactiveButtonFontColor,
       backgroundColor:unactiveButtonColor,
@@ -57,9 +72,28 @@ function Grid() {
   const classes = useStyles()
 
   const [isSimulationRunning, setSimulationRunning] = useState(false)
+  const [speed, setSpeed] = useState(200)
+
+  const handleChange = (e, newSpeed) => {
+    setSimulationRunning(false)
+    setSpeed(newSpeed)
+  }
+
+  const handleSlowest = () => {
+    setSimulationRunning(false)
+    setSpeed(1)
+  }
+
+  const handleFastest = () => {
+    setSimulationRunning(false)
+    setSpeed(1001)
+  }
 
   const runningRef = useRef(isSimulationRunning)
   runningRef.current = isSimulationRunning
+
+  const speedRef = useRef(speed)
+  speedRef.current = speed
 
   // console.log(grid)
 
@@ -91,9 +125,10 @@ function Grid() {
       })
     })
 
-    setTimeout(runSimulation, 100)
+    const simSpeed = (1000/speed)
+    setTimeout(runSimulation, simSpeed)
 
-  }, [])
+  }, [speed])
 
 
   return (
@@ -120,13 +155,49 @@ function Grid() {
       <Button className={classes.booton} variant="contained" onClick={()=> {
         const rows = []
         for (let i = 0; i < numOfRows; i++) {
-          rows.push(Array.from(Array(numOfColumns), () => Math.random() > 0.7 ? 1 : 0))
+          rows.push(Array.from(Array(numOfColumns), () => Math.random() > 0.8 ? 1 : 0))
         }
         setGrid(rows)
         }}
       >
         Randomize Grid
       </Button>
+      
+      <SpeedGrid className={classes.root} container spacing={2}>
+        <SpeedGrid item>
+          <img 
+            className={speedRef.current === 1 ? "activatedSnorlaxIcon" : "snorlaxIcon"} 
+            src={Snorlax} 
+            alt="Slow like Snorlax"
+            onClick={()=> {
+              handleSlowest()
+            }} 
+          />
+        </SpeedGrid>
+        <SpeedGrid>
+          <Slider
+            className={classes.slider}
+            defaultValue={speed}
+            value={speedRef.current}
+            aria-labelledby="discrete-slider"
+            step={200}
+            marks
+            min={1}
+            max={1001}
+            onChange={handleChange}
+          />
+        </SpeedGrid>
+        <SpeedGrid item>
+          <img 
+            className={speedRef.current === 1001 ? "activatedSonicIcon" : "sonicIcon"} 
+            src={Sonic} 
+            alt="Fast like Sonic"
+            onClick={()=> {
+              handleFastest()
+            }}  
+          />
+        </SpeedGrid>
+      </SpeedGrid>
     </div>
     
     <div className="Grid" 
